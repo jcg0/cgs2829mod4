dbc6fbb44df5379fd275471c06ba628990e1667f
Jefferson Griebel <jg.awsprod@gmail.com>
2026-02-13 20:51:39 -0500

    added click event for a images

---
+++ carousel.js
@@ -0,0 +1,59 @@
+"use strict";
+
+$(document).ready( () => {
+	
+	const slider = $("#image_list");                     // slider = ul element
+	let leftProperty = 0;
+	let newLeftProperty = 0;
+		
+	// the click event handler for the right button						
+	$("#right_button").click( () => { 
+		// get value of current left property
+		leftProperty = parseInt(slider.css("left"));
+		
+		// determine new value of left property
+		if (leftProperty - 300 <= -900) {
+			newLeftProperty = 0; }
+		else {
+			newLeftProperty = leftProperty - 300; 
+		}
+		
+		// use the animate function to change the left property
+		slider.animate( {left: newLeftProperty}, 1000);
+	});  // end click
+	
+	// the click event handler for the left button
+	$("#left_button").click( () => {
+		// get value of current right property
+		leftProperty = parseInt(slider.css("left"));
+		
+		// determine new value of left property
+		if (leftProperty < 0) {
+			newLeftProperty = leftProperty + 300;
+		} else {
+			newLeftProperty = 0;
+		}
+		
+		// use the animate function to change the left property
+		slider.animate( {left: newLeftProperty}, 1000);				
+	});  // end click	
+
+	$('#image_list a').click( (e) => {
+		e.preventDefault();
+		const imageURL = $('#image');
+
+		const bigImage = $(this).attr('href');
+		console.log(bigImage);
+		console.log('booliallie');
+		imageURL.stop(true, true);
+
+		imageURL.animate({opacity: 0, marginLeft: '-=205'}, 1000, 
+			() => {
+				imageURL.attr('src', bigImage);
+				imageURL.css('margin-left', '310px');
+				imageURL.animate({opacity: 1, marginLeft: '-=205'}, 1000);
+			}
+		);
+	})
+	
+}); // end ready
